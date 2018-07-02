45cb6d45900b65f8a4036bc4e08c9159dacda67f
Angsuman Ray <angsumanray@outlook.com>
2018-07-03 05:22:20 +0530

    Added login and dashboard components

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule
   ],
-  declarations: []
+  declarations: [LoginComponent, DashboardComponent]
 })
 export class AdminModule { }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
     CommonModule,
     UserRoutingModule
   ],
-  declarations: []
+  declarations: [LoginComponent, DashboardComponent]
 })
 export class UserModule { }
