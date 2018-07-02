98c82b46c24a908631dd63d5ab42c2d26cbf29a2
Angsuman Ray <angsumanray@outlook.com>
2018-07-03 05:17:35 +0530

    Updated the structure.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: 'app/admin/admin.module#AdminModule'
+  },
+  {
+    path: '',
+    loadChildren: 'app/user/user.module#UserModule'
+  }
+];
 
 @NgModule({
   imports: [
